69467ce4c7192e8a20365062747da29e94f9b304
Ferhat Minder <ferhatmndr@gmail.com>
2021-11-05 11:12:45 +0300

    should list stock prices

---
+++ app/src/androidTest/java/com/ferhatminder/stocks/End2EndTest.kt
@@ -0,0 +1,34 @@
+package com.ferhatminder.stocks
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+
+@LargeTest
+class End2EndTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun e2eTest() {
+        shouldListStockPrices()
+    }
+
+    private fun shouldListStockPrices() {
+        val stockPrices = listOf(
+            StockPrice("GARAN", 9.76),
+            StockPrice("THYAO", 13.26)
+        )
+        for (stockPrice in stockPrices) {
+            composeTestRule.onNodeWithText(stockPrice.code).assertIsDisplayed()
+            composeTestRule.onNodeWithText(stockPrice.price.toString()).assertIsDisplayed()
+        }
+    }
+
+    data class StockPrice(val code: String, val price: Double)
+
+}
